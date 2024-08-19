62f2aefaa77cbf880460f393998e2d712fc02547
gamberfox <gamberfox@gmail.com>
2024-08-19 13:45:02 -0500

    Squashed commit of the following:
    commit 9554c8f0b202ec710d4eddaad4d16e26df989c06
    Merge: 0aac72b 0c72e6e
    Author: gamberfox <gamberfox@gmail.com>
    Date:   Mon Aug 19 13:39:06 2024 -0500
    
        Merge branch 'dev' of github.com:gamberfox/pragma-bootcamp-project into dev
    
    commit 0aac72b9e798865a361b44051942a6cb51cd29f3
    Author: gamberfox <gamberfox@gmail.com>
    Date:   Mon Aug 19 13:22:53 2024 -0500
    
        avances HU-001

---
+++ stock-api-service/src/test/java/com/emazon/stock_api_service/StockApiServiceCategoryTest.java
@@ -0,0 +1,25 @@
+package com.emazon.stock_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class StockApiServiceCategoryTest {
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void categoryTest(){
+        int a=2;
+        int b=2;
+        assertEquals(2,a);
+    }
+    @Test
+    void getCategoryTest2() {
+        int a=2;
+        int b=2;
+        assertEquals(4,a+b);
+    }
+}
