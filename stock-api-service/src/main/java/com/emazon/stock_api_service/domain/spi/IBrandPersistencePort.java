56cc21de975fb26fe6abc573faceb015326b133d
gamberfox <gamberfox@gmail.com>
2024-08-20 16:15:45 -0500

    Squashed commit of the following:
    commit 74f9c9aafdfba724279804c724657779ecb3d276
    Author: gamberfox <gamberfox@gmail.com>
    Date:   Tue Aug 20 16:14:56 2024 -0500
    
        HU-003 progress

---
+++ stock-api-service/src/main/java/com/emazon/stock_api_service/domain/spi/IBrandPersistencePort.java
@@ -0,0 +1,11 @@
+package com.emazon.stock_api_service.domain.spi;
+
+import com.emazon.stock_api_service.domain.model.Brand;
+
+import java.util.List;
+
+public interface IBrandPersistencePort {
+    void createBrand(Brand brand);
+    Brand getBrand(Long brandName);
+    List<Brand> getBrands(Boolean ascendingOrder, int page, int size);
+}
