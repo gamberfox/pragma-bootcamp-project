cdc0f6ae7628271a46cf3805a889a4f16a058783
gamberfox <gamberfox@gmail.com>
2024-08-20 05:59:38 -0500

    avances HU-001

--- stock.sql
+++ stock.sql
@@ -8,26 +8,26 @@ USE stock;
 -- brand table
 DROP TABLE IF EXISTS brand;
 CREATE TABLE brand (
-    id_brand INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) UNIQUE,
     description VARCHAR(120)
 );
 -- category table
 DROP TABLE IF EXISTS category;
 CREATE TABLE category (
-    id_category INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) unique,
     description VARCHAR(90) NOT NULL
 );
 -- article table
 DROP TABLE IF EXISTS article;
 CREATE TABLE article (
-    id_article INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) unique,
     stock_quantity INT,
     price decimal(10, 2),
     id_brand INT,
-    FOREIGN KEY (id_brand) REFERENCES brand(id_brand)
+    FOREIGN KEY (id_brand) REFERENCES brand(id)
 );
 -- article_category table
 DROP TABLE IF EXISTS article_category;
@@ -35,8 +35,8 @@ CREATE TABLE article_category (
     id_article INT NOT NULL,
     id_category INT NOT NULL,
     PRIMARY KEY (id_article, id_category),
-    FOREIGN KEY (id_article) REFERENCES article(id_article),
-    FOREIGN KEY (id_category) REFERENCES category(id_category)
+    FOREIGN KEY (id_article) REFERENCES article(id),
+    FOREIGN KEY (id_category) REFERENCES category(id)
 );
 INSERT INTO brand(name, description)
 VALUES('noke', 'niko brand description');
