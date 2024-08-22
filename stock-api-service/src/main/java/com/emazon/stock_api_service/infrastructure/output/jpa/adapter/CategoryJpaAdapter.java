96b9eba5321acd80404e424f7a00966d980e7117
gamberfox <gamberfox@gmail.com>
2024-08-22 12:04:18 -0500

    HU-001 merge issues there's an incredible amount of bad things that happen when you trust git to automerge

--- stock-api-service/src/main/java/com/emazon/stock_api_service/infrastructure/output/jpa/adapter/CategoryJpaAdapter.java
+++ stock-api-service/src/main/java/com/emazon/stock_api_service/infrastructure/output/jpa/adapter/CategoryJpaAdapter.java
@@ -5,34 +5,11 @@ import com.emazon.stock_api_service.domain.exception.ErrorType;
 import com.emazon.stock_api_service.domain.exception.category.CategoryException;
 import com.emazon.stock_api_service.domain.model.Category;
 import com.emazon.stock_api_service.domain.spi.ICategoryPersistencePort;
-import com.emazon.stock_api_service.infrastructure.exception.CategoryNotFoundException;
+import com.emazon.stock_api_service.infrastructure.exception.CategoryPersistenceException;
 import com.emazon.stock_api_service.infrastructure.output.jpa.entity.CategoryEntity;
 import com.emazon.stock_api_service.infrastructure.output.jpa.mapper.ICategoryEntityMapper;
 import com.emazon.stock_api_service.infrastructure.output.jpa.repository.ICategoryRepository;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
-import java.util.List;
-
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -42,13 +19,21 @@ public class CategoryJpaAdapter implements ICategoryPersistencePort {
 
     @Override
     public void createCategory(Category category) {
+        if(categoryRepository.findByName(category.getName()).isPresent()) {
+            throw new CategoryPersistenceException(
+                    "VALIDATION ERROR: the category name "
+                    +category.getName()+" already exists");
+        }
         categoryRepository.save(categoryEntityMapper.toEntity(category));
     }
 
     @Override
     public Category getCategory(Long id) {
         CategoryEntity categoryEntity = categoryRepository.findById(id)
-                .orElseThrow(()->new CategoryException(ErrorType.RESOURCE_NOT_FOUND,"category id does not exist"));
+                .orElseThrow(()->new CategoryPersistenceException(
+                        "category id "
+                                +id.toString()
+                                +" does not exist"));
         return categoryEntityMapper.toCategory(categoryEntity);
         //public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long> {
         //.orElseThrow(() -> new CategoryNotFoundException())); also works
